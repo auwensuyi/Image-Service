48adc4e61158635558d2deeb0768955cb144d39b
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2026-03-05 16:59:53 -0600

    Added iam user and policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,8 +14,41 @@ provider "aws" {
 
 # Example: Create an S3 bucket
 resource "aws_s3_bucket" "example_bucket_test" {
-  bucket = "my-unique-terraform-bucket-name-based-god" # Must be globally unique
+  bucket = "testing-with-onehpee-and-mkultra-bucket" # Must be globally unique
   tags = {
     Project = "Terraform AWS Connection Demo"
   }
+}
+
+resource "aws_iam_user" "image-uploader" {
+  name = "image-uploader-test"
+  path = "/"
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+
+resource "aws_iam_access_key" "image-uploader-access-key" {
+  user = aws_iam_user.image-uploader.name
+}
+
+data "aws_iam_policy_document" "image-uploader-ro" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:ListAllMyBuckets", "s3:GetBucketLocation", "s3:PutObject", "s3:GetObject", "s3:DeleteObject", "s3:ListBucket"]
+    resources = [
+      "*",
+      "arn:aws:s3:::image-service-bucket-based",
+      "arn:aws:s3:::image-service-bucket-based/*",
+      "arn:aws:s3:::thumbnail-image-service",
+      "arn:aws:s3:::thumbnail-image-service/*"
+    ]
+  }
+}
+
+resource "aws_iam_user_policy" "lb_ro" {
+  name   = "testing-policy-with-onehpee-and-mkultra"
+  user   = aws_iam_user.image-uploader.name
+  policy = data.aws_iam_policy_document.image-uploader-ro.json
 }
